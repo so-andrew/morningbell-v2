af0d339dd7f41e08d7cfc3d4c4552080d44d11a4
Andrew So <so.andrew@ufl.edu>
2024-05-15 03:01:23 -0400

    Chat autoscroll to bottom on new message (test implementation)

--- packages/frontend/app/rooms/[id]/ChatMessage.tsx
+++ packages/frontend/app/rooms/[id]/ChatMessage.tsx
@@ -5,7 +5,7 @@ export default function ChatMessageItem(params: {element: ChatMessage | LogMessa
 
     return (
         <>
-            <div className={`group bg-white ${element.username ? 'text-black' : 'text-gray-500'}`}>{element.username && (<><span className='font-bold'>{element.username}</span><span className='font-normal'>: </span></>)} <span className={`${element.username ? 'not-italic' : 'italic'} break-words`}>{element.content}</span><span className='bg-white text-white group-hover:text-gray-500 text-sm'> ({new Date(element.timestamp).toLocaleTimeString('en-US')})</span></div>
+            <div className={`group bg-white ${element.username ? 'text-black' : 'text-gray-500'} sm:[overflow-anchor:none] last-of-type:[overflow-anchor:auto]`}>{element.username && (<><span className='font-bold'>{element.username}</span><span className='font-normal'>: </span></>)} <span className={`${element.username ? 'not-italic' : 'italic'} break-words`}>{element.content}</span><span className='bg-white text-white group-hover:text-gray-500 text-sm'> ({new Date(element.timestamp).toLocaleTimeString('en-US')})</span></div>
         </>
     );
 }
